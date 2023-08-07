da89eca2e6d027f3095460e3ddf72c769112d89c
AndriiLevin <andriilevin@gmail.com>
2023-08-07 20:20:06 +0100

    --init --

--- app/app.py
+++ app/app.py
@@ -1,17 +1,24 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 import logging
 
-from config import Configuration
+from flask import Flask
+from flask_migrate import Migrate, MigrateCommand
 
+
+from flask.cli import FlaskGroup
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Configuration
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+manager = FlaskGroup(app)
+manager.add_command('db', MigrateCommand)
 
- #Настройка логирования
+# Настройка логирования
 logging.basicConfig(level=logging.DEBUG)  # Устанавливаем уровень логирования
 
 # Создаем объект логгера для SQLAlchemy
