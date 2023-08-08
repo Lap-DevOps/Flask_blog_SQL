994b90131cfedb4b6ccdd91accf894774a115af0
AndriiLevin <andriilevin@gmail.com>
2023-08-08 17:35:59 +0100

    -- adminka

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
 import logging
 
 from flask import Flask
-from flask_migrate import Migrate, MigrateCommand
-
-
 from flask.cli import FlaskGroup
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_migrate import Migrate, MigrateCommand
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Configuration
@@ -17,6 +17,14 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = FlaskGroup(app)
 manager.add_command('db', MigrateCommand)
+
+from models import Post, Tag
+
+admin = Admin(app)
+admin.add_view(ModelView(Post, db.session))
+admin.add_view(ModelView(Tag, db.session))
+
+
 
 # Настройка логирования
 logging.basicConfig(level=logging.DEBUG)  # Устанавливаем уровень логирования
--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -40,11 +40,11 @@ def index():
     if q:
         posts_query = Post.query.filter(Post.title.contains(q) | Post.body.contains(q))  # .all()
     else:
-        posts_query = Post.query.order_by(Post.created.desc()) #.all()
+        posts_query = Post.query.order_by(Post.created.desc())  # .all()
 
-    pages = posts_query.paginate(page=page, per_page=2)
+    pages = posts_query.paginate(page=page, per_page=10)
 
-    return render_template('posts/index.html',  pages=pages)
+    return render_template('posts/index.html', pages=pages)
 
 
 @posts.route("/<slug>")
@@ -59,3 +59,19 @@ def tag_detail(slug):
     tag = Tag.query.filter(Tag.slug == slug).first()
     posts = tag.posts.all()
     return render_template('posts/tag_detail.html', tag=tag, posts=posts)
+
+
+@posts.route('/<slug>/edit/', methods=["POST", "GET"])
+def edit_post(slug):
+    post = Post.query.filter(Post.slug == slug).first()
+
+    if request.method == "POST":
+        form = PostForm(formdata=request.form, obj=post)
+        form.populate_obj(post)
+        db.session.commit()
+
+        return redirect(url_for('posts.post_detail', slug=post.slug))
+
+    form = PostForm(obj=post)
+
+    return render_template('posts/edit.html', post=post, form=form)
